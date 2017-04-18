cc3a78d7312f3c171a721b438faa27ac791eb6c8
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-04-18 06:31:55 +0200

    File Uploading
    - File Uploaded using ('express-fileupload') module

--- DeveSolution/DeveSolution/server.js
+++ DeveSolution/DeveSolution/server.js
@@ -1,13 +1,19 @@
-'use strict';
+
 var express = require('express');
+
 var path = require('path');
 var app = express();
+var BodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
+//var Busboy = require('busboy');
+
+
 
 
 
 app.set("view engine", "ejs");
-
-
+app.use(BodyParser());
+app.use(fileUpload());
 
 app.get("/", function (req, res) {
     res.render(__dirname + "/Pages/Home.ejs")
@@ -23,9 +29,36 @@ app.get("/login", function (req, res) {
     res.render(__dirname + "/Pages/login.ejs");
 })
 
+
+app.get("/Upload", function (req, res) {
+    res.render(__dirname + "/Pages/Upload.ejs");
+})
+
 app.get("/signup.html", function (req, res) {
     console.log("Home");
     res.sendFile(__dirname + "/HomeView.html");
+})
+
+
+// File Uploading ... 
+app.post("/UploadFile", function (req, res) {
+        console.log("FileToBeUploaded");
+        console.log(req.files.VLogo.name);
+
+        sampleFile = req.files.VLogo;
+
+        uploadPath = __dirname + '/Repositorie/' + sampleFile.name;
+
+        sampleFile.mv(uploadPath, function (err) {
+            if (err)
+                return res.status(500).send(err);
+
+            res.send('File uploaded to ' + uploadPath);
+        });
+        //console.log(req.files.Source);
+        //console.log(req.files.ALogo);
+        console.log(req.body.Vsite);
+
 })
 
 
